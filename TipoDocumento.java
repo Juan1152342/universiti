3e6354e0dac324707190eeeb403040b8f81fe096
juan roddriguez <juandavidroro@ufps.edu.co>
2024-05-30 18:01:17 -0500

    Intercambio inicial de proyecto

---
+++ TipoDocumento.java
@@ -0,0 +1,11 @@
+
+/**
+ * Write a description of class tipoDocumento here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public enum TipoDocumento {
+    CC,TI,RC,CE,NUIP,PS;
+}
+
